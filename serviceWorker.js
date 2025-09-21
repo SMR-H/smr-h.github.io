daa72b4cbf4ca220a022e39e616b6a433292efe6
Seyed Mohammad Reza Hosseini <s.mohamadr.h@gmail.com>
2025-09-22 00:01:07 +0330

    Initial commit

---
+++ serviceWorker.js
@@ -0,0 +1,47 @@
+const staticMohammadData = "site-cache-v1";
+const assets = [
+  //   "/",
+  "./index.html",
+  "./assets/css/styles.css",
+  "./assets/css/swiper-bundle.min.css",
+  "./assets/js/main.js",
+  "./assets/js/swiper-bundle.min.js",
+
+  "./assets/pdf/cv-en_fa-seyed_mohammad_reza_hosseini.pdf",
+
+  "./assets/img/blob.svg",
+  "./assets/img/smrh-3.png",
+  "./assets/img/cozy-gaming.png",
+  "./assets/img/travel-pic3.png",
+
+  "./assets/img/skills/python.svg",
+  "./assets/img/skills/django-bright.svg",
+  "./assets/img/skills/djangorest.svg",
+  "./assets/img/skills/fastapi.svg",
+  "./assets/img/skills/redis.svg",
+  "./assets/img/skills/postgresql.svg",
+  "./assets/img/skills/mongodb.svg",
+  "./assets/img/skills/docker.svg",
+  "./assets/img/skills/nginx.svg",
+
+  "./assets/img/portfolio/uama.jpg",
+  "./assets/img/portfolio/Private_Chat.jpg",
+  "./assets/img/portfolio/Telegram_Channel_Output.jpg",
+  "./assets/img/portfolio/Telegram_Adder.jpg",
+];
+
+self.addEventListener("install", (installEvent) => {
+  installEvent.waitUntil(
+    caches.open(staticMohammadData).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", (fetchEvent) => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then((res) => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
